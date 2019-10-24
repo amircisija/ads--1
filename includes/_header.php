906e2da9d81cf3fb8b43dca509ad3a9f7e5e1c02
katakx <amircisija@gmail.com>
2019-10-24 13:57:15 +0200

    initial commit

---
+++ includes/_header.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <script src="https://kit.fontawesome.com/fab936345c.js" crossorigin="anonymous"></script>
+    <link
+        href="https://fonts.googleapis.com/css?family=Fira+Sans:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <title>Klick Media | Ihre Online Marketing Agentur</title>
+</head>
+
+<body>
+    <div id="top__bar--navigation">
+        <div class="top--info--bar" id="top--bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-lg-8 ml-auto text-right">
+                        <ul class="top--contact--info">
+                            <li><i class="fas fa-map-marked-alt"></i> <span>Adresse:</span> Muster Adresse 99, 99999 Ort
+                            </li>
+                            <li><i class="fas fa-phone-square-alt"></i> <span>Telefon:</span> + 123 456 789</li>
+                            <li><i class="fas fa-envelope-square"></i> <span>E-Mail:</span> info@domain.de</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <header class="main--header fixed-top" id="header--top">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#"><img src="assets/img/logo-big.png" class="img-fluid" width="280"
+                            alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/">Startseite <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#ueber-uns">Über uns</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#unsere-leistungen" id="navbarDropdown" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Leistungen
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/google-ads">Google Ads</a>
+                                    <a class="dropdown-item" href="/sma">Social Network Ads</a>
+                                    <a class="dropdown-item" href="/webseiten">Webseiten</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#kontakt">Kontakt</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
---
+++ index.php
@@ -0,0 +1,363 @@
+<?php 
+include ('includes/_header.php');
+?>
+    <div class="clearfix"></div>
+    <section class="hero--section">
+        <img src="assets/img/el--1.png" class="slide--1 sc--1" alt="">
+        <img src="assets/img/el--2.png" class="slide--1 sc--2" alt="">
+        <img src="assets/img/el--3.png" class="slide--1 sc--3" alt="">
+        <img src="assets/img/el--4.png" class="slide--1 sc--4" alt="">
+        <img src="assets/img/el--5.png" class="slide--1 sc--5" alt="">
+        <img src="assets/img/el--6.png" class="slide--1 sc--6" alt="">
+        <img src="assets/img/el--7.png" class="slide--1 sc--7" alt="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2 class="mb-4">Klick-Media.de</h2>
+                    <p class="lead mb-5">Wir <strong>verbessern die Online-Sichtbarkeit</strong> Ihres Unternehmens mit
+                        verschiedenen
+                        Onlinemarketing- und Werbediensten: Google Ads, Facebook Ads, Instagram Ads, Google Tag Manager,
+                        Implementierung von Analyse und Tracking-Tags.</p>
+                    <a href="#" class="btn btn-default btn-primary btn-circle mr-3">Über uns</a>
+                    <a href="#" class="btn btn-default btn-primary btn-circle">Leistungen</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="page--section pg-1">
+        <img src="assets/img/ic--1.png" class="ic-slide--1 ic--1" alt="">
+        <img src="assets/img/ic--2.png" class="ic-slide--1 ic--2" alt="">
+        <img src="assets/img/ic--3.png" class="ic-slide--1 ic--3" alt="">        
+        <img src="assets/img/ic--4.png" class="ic-slide--1 ic--4" alt="">  
+        <img src="assets/img/ic--2.png" class="ic-slide--1 ic--5" alt="">                    
+        <div class="container">
+            <div class="row wow fadeInUp">
+                <div class="col-lg-10 mx-auto text-center">
+                    <img src="assets/img/g-ads-logo.png" width="100" class="mb-4" alt="">
+                    <h1>
+                        Wir sind der Richtige <span class="st--1">Google Ads</span> Partner
+                    </h1>
+                    <hr class="hr-style-1">
+                    <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam odio vitae tempora
+                        totam modi? Ea dolore modi enim reiciendis a deserunt similique qui. Ex numquam optio obcaecati
+                        amet libero neque.</p>
+                </div>
+            </div>
+            <div class="row intro--section py-5">
+                <div class="col-lg-6">
+                    <div class="card shadow--2 wow fadeInUp">
+                        <div class="row">
+                            <div class="col-lg-3 my-auto mx-auto text-center">
+                                <img src="assets/img/icon--1.png" class="img-fluid" width="100" alt="">
+                            </div>
+                            <div class="col-lg-9 p-4 my-auto">
+                                <h4>Sparen Sie Zeit, Ressourcen und Geld!</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, sed inventore
+                                    facere
+                                    modi voluptatibus error voluptate quae quidem </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card shadow--2 wow fadeInUp">
+                        <div class="row">
+                            <div class="col-lg-3 my-auto mx-auto text-center">
+                                <img src="assets/img/icon--2.png" class="img-fluid" width="100" alt="">
+                            </div>
+                            <div class="col-lg-9 p-4 my-auto">
+                                <h4>Kompetente Beratung</h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiror voluptate quae quidem dicta dolores
+                                    saepe perspiciatis
+                                    voluptates
+                                    molestias hic eos dolor reiciendis, blanditiis vitae.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card shadow--2 wow fadeInUp">
+                        <div class="row">
+                            <div class="col-lg-3 my-auto mx-auto text-center">
+                                <img src="assets/img/icon--3.png" class="img-fluid" width="100" alt="">
+                            </div>
+                            <div class="col-lg-9 p-4 my-auto">
+                                <h4>Schaffen Sie endlose Geschäftsmöglichkeiten!</h4>
+                                <p>Lorem ipsum us error voluptate quae quidem dicta dolores saepe perspiciatis
+                                    voluptates
+                                    molestias hic eos dolor reiciendis, blanditiis vitae.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 my-auto rel-pos">
+                    <div data-jarallax-element="-100">
+                        <img src="assets/img/el--8.png" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="page--section" id="cta--section--1">
+        <div class="container" id="ueber-uns">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card  bg-white shadow--1 p-4">
+                        <img src="assets/img/img__1.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 my-auto p-5">
+                    <h2>Über uns</h2>
+                    <p>
+                        <strong>Klick Media</strong> unterstützt Unternehmen bei Ihrer Kundenakquise und steigert Ihre
+                        Verkaufschancen
+                        im Onlinebereich.
+                        In unserem Unternehmen legen wir Wert auf qualitativ hochwertigen Service weshalb wir
+                        ausschließlich Experten beschäftigen, die als Ziel haben, alle Kundenprojekte zur vollsten
+                        Zufriedenheit zu beenden.
+
+                    </p>
+                    <p>
+                        Unser Ziel ist es, Ihr Unternehmen zur bestmöglichsten und effizientesten Positionierung zu
+                        katapultieren. Hierfür unterstützen Sie unsere Kampagnenmanager umfassend und beraten Sie zu den
+                        besten Keywords für Ihre Google Ads Kampagne.
+                    </p>
+
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-lg-6 my-auto p-5">
+                    <p>
+                        Um effektiv Kunden generieren zu können, ist es wichtig Ihre Zielgruppe genau zu kennen!
+                        Hierbei unterstützen wir Sie gerne umfassend. Wir analysieren Ihre Zielgruppe und schalten Ihre
+                        Kampagne in der effektivsten Region.
+
+                    </p>
+                    <p>
+                        Nutzen Sie die richtigen Keywords. Oft kommt es vor das Keywords nur Aufgrund Ihrer Beliebtheit
+                        verwendet werden. Stellen Sie sicher, dass Sie die bestmöglichen Keywords verwenden, die für Ihr
+                        Unternehmen geeignet sind. Wir helfen!
+                    </p>
+                    <p>
+                        Wir sind der Richtige Google Ads Partner, gerne besprechen wir Ihre Wünsche und Vorstellungen
+                        sodass wir ein gemeinsames Ziel optimal und zeitnah erreichen können: mehr Anrufe, mehr Kunden,
+                        mehr Umsatz.
+                    </p>
+                </div>
+                <div class="col-lg-6 text-center my-auto">
+                    <div class="card bg-white shadow--1 p-4 pt-4">
+                        <img src="assets/img/img__4.jpg" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="page--section services--section" id="unsere-leistungen">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p class="lead mb-0">Professionnel & Kompetent</p>
+                    <h2>Unsere Leistungen</h2>
+                </div>
+            </div>
+            <div class="row py-5">
+                <div class="col-lg-4">
+                    <div class="card text-center shadow--2 p-5" style="background-image: url(assets/img/img__5.jpg);">
+                    <div class="overlay--shadow"></div>
+                    <div class="card--inner--content">
+                        <img src="assets/img/icon--4.png" class="img-fluid mx-auto mb-4" width="100" alt="">
+                        <h4 class="mb-4">Google Ads</h4>
+                        <p>PPC Kampagnen bei der Suchmaschine von Google, Shopping-Kampagnen für E-Commerce,
+                            Display-Kampagnen und
+                            YouTube-Videoanzeigen.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card text-center shadow--2 p-5" style="background-image: url(assets/img/img__6.jpg);">
+                    <div class="overlay--shadow"></div>
+                    <div class="card--inner--content">
+                        <img src="assets/img/icon--5.png" class="img-fluid mx-auto mb-4" width="100" alt="">
+                        <h4 class="mb-4">Social Network Ads</h4>
+                        <p>Werbekampagnen in sozialen Netzwerken: Facebook-Anzeigen, Instagram-Anzeigen, Twitter-Anzeigen, Linkedin-Anzeigen uvm.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card text-center shadow--2 p-5" style="background-image: url(assets/img/img__7.jpg);">
+                    <div class="overlay--shadow"></div>
+                    <div class="card--inner--content">
+                        <img src="assets/img/icon--6.png" class="img-fluid mx-auto mb-4" width="100" alt="">
+                        <h4 class="mb-4">Webseiten</h4>
+                        <p>Erstellung oder Bearbeitung von Webseiten sowie Erstellung und Optimierung von Landingpages für Werbekampagnen, Soziale Netzwerke oder lokale Aktivitäten für Geschäfte wie Flyer, Banner und jede Art von Grafik Design.
+</p>
+                            </div>
+                    </div>
+                </div>                
+            </div>
+        </div>
+    </section>
+    <section class="page--section contact--section" id="kontakt">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <form method="post">
+                        <div class="row">
+                            <div class="col-lg-6 my-auto">
+                                <div class="contact--section--info">
+                                    <h2>Kontakt</h2>
+                                    <p>
+                                        Rufen Sie uns an oder schicken Sie uns eine E-Mail.
+                                    </p>
+                                    <hr>
+                                    <ul>
+                                        <li class="d-block"><i class="fas fa-map-marked-alt"></i> <br>
+                                            <strong>Adresse:</strong>
+                                            <br><span>
+                                                Muster Adresse
+                                                99, 99999 Ort</span>
+                                        </li>
+                                        <li class="d-block"><i class="fas fa-phone-square-alt"></i><br>
+                                            <strong>Telefon:</strong> <br><span>+ 123 456 789</span>
+                                        </li>
+                                        <li class="d-block"><i class="fas fa-fax"></i><br>
+                                            <strong>Fax:</strong><br><span>+ 123 456 789</span>
+                                        </li>
+                                        <li class="d-block"> <i class="fas fa-envelope-square"></i><br>
+                                            <strong>E-Mail:</strong><br>
+                                            <span>
+                                                info@domain.de
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-6 form--container my-auto">
+
+                                <div class="form-group">
+                                    <input type="text" name="txtName" class="form-control" placeholder="Name *"
+                                        value="" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="txtEmail" class="form-control" placeholder="E-Mail *"
+                                        value="" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="txtPhone" class="form-control"
+                                        placeholder="Telefonnummer *" value="" />
+                                </div>
+
+                                <div class="form-group">
+                                    <textarea name="txtMsg" class="form-control" placeholder="Nachricht *"
+                                        style="width: 100%; height: 150px;"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" name="btnSubmit"
+                                        class="btn btn-default btn-primary btn-circle mr-3"
+                                        value="Nachricht Abschicken" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Alle Rechte Vorbehalten</p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="assets/js/jarallax.min.js"></script>
+    <script src="assets/js/jarallax-element.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/utils/Draggable.min.js"></script>
+    <script>
+        new WOW().init();
+        $('.jarallax').jarallax({
+            speed: 0.2
+        });
+        var timeout;
+        $('.hero--section').mousemove(function (e) {
+            if (timeout) clearTimeout(timeout);
+            setTimeout(callParallax.bind(null, e), 200);
+
+        });
+        $('.pg-1').mousemove(function (e) {
+            if (timeout) clearTimeout(timeout);
+            setTimeout(callParallax.bind(null, e), 200);
+
+        });        
+
+        function callParallax(e) {
+            parallaxIt(e, '.slide--1', -100);
+            parallaxIt(e, '.sc--2', -30);
+            parallaxIt(e, '.sc--3', -50);
+            parallaxIt(e, '.sc--4', -10);
+            parallaxIt(e, '.sc--5', -5);
+            parallaxIt(e, '.sc--6', -20);
+            parallaxIt(e, '.sc--7', 20);
+            parallaxItTwo(e, '.ic--1', -100);
+            parallaxItTwo(e, '.ic--2', 10);   
+            parallaxItTwo(e, '.ic--3', 40);         
+            parallaxItTwo(e, '.ic--4', -40);   
+            parallaxItTwo(e, '.ic--5', 140);                                       
+        }
+        function parallaxItTwo(e, target, movement) {
+            var $this = $('.pg-1');
+            var relX = e.pageX - $this.offset().left;
+            var relY = e.pageY - $this.offset().top;
+
+            TweenMax.to(target, 1, {
+                x: (relX - $this.width() / 2) / $this.width() * movement,
+                y: (relY - $this.height() / 2) / $this.height() * movement,
+                ease: Power2.easeOut
+            })
+        };
+        function parallaxIt(e, target, movement) {
+            var $this = $('.hero--section');
+            var relX = e.pageX - $this.offset().left;
+            var relY = e.pageY - $this.offset().top;
+
+            TweenMax.to(target, 1, {
+                x: (relX - $this.width() / 2) / $this.width() * movement,
+                y: (relY - $this.height() / 2) / $this.height() * movement,
+                ease: Power2.easeOut
+            })
+        };
+
+        /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function () {
+            var currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                document.getElementById("top--bar").style.top = "0";
+                document.getElementById("header--top").style.top = "43px";
+            } else {
+                document.getElementById("top--bar").style.top = "-50px";
+                document.getElementById("header--top").style.top = "0";
+            }
+            prevScrollpos = currentScrollPos;
+        }
+    </script>
+</body>
+
+</html>
